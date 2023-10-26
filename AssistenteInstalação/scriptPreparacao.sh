37376ccb8dca3abdadf865d238ee06a04a9aa594
kaori_katayama <125297000+Kaori2@users.noreply.github.com>
2023-10-26 16:58:04 -0300

    Update scriptPreparacao.sh

--- AssistenteInstalação/scriptPreparacao.sh
+++ AssistenteInstalação/scriptPreparacao.sh
@@ -6,12 +6,9 @@ sleep 5
 echo “[Assistente PowerTech]: Preparando a clonação da pasta...”
 sleep 5
 
-git clone https://github.com/PowerTechSolutions/PowerTechSolutions-Documentacao.git
+git clone https://github.com/PowerTechSolutions/PowerTechSolutions-Documentacao/tree/30825f4f6d84475db632966dfacd93e26d932524/AssistenteInstala%C3%A7%C3%A3o
 
 echo “[Assistente PowerTech]: Clonação com sucesso! Agora acessando o diretorio”
-sleep 5
-
-cd PowerTechSolutions-Documentacao
 sleep 5
 
 echo "[Assistente PowerTech]: Começando a primeira passo!"
